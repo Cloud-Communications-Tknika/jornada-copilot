030fa84ffa5c8e966d2d408310efe587d6b4675c
prubioamzubiri <prubioam@zubirimanteo.com>
2023-11-30 11:08:42 +0000

    Fichero para crear funciones creada

---
+++ Ejemplo1/CrearFunciones.java
@@ -0,0 +1,22 @@
+package Ejemplo1;
+
+public class CrearFunciones {
+    public static void main(String[] args) {
+        
+        int a[] = {1, 2, 3, 4, 5};
+
+        System.out.println("La suma de los elementos del arreglo es: " + suma(a));
+        System.out.println("El promedio de los elementos del arreglo es: " + promedio(a));
+        System.out.println("El mayor de los elementos del arreglo es: " + mayor(a));
+        System.out.println("El menor de los elementos del arreglo es: " + menor(a));
+        System.out.println("El arreglo invertido es: ");
+        invertir(a);
+        System.out.println("El arreglo ordenado es: ");
+        ordenar(a);
+    }
+}
+    
+    
+
+
+
