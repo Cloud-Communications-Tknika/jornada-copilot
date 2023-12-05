ba61a03a2cc034e90961f676458c2f30001d2451
prubioamzubiri <prubioam@zubirimanteo.com>
2023-12-05 09:12:46 +0000

    cambiado array de crear funciones

--- Ejemplo1/CrearFunciones.java
+++ Ejemplo1/CrearFunciones.java
@@ -3,7 +3,7 @@ package Ejemplo1;
 public class CrearFunciones {
     public static void main(String[] args) {
         
-        int a[] = {1, 2, 3, 4, 5};
+        int a[] = {7, 14, 9, 18, 11, 6, 13, 12, 1, 5, 4, 3, 2, 8, 10};
 
         System.out.println("La suma de los elementos del arreglo es: " + suma(a));
         System.out.println("El promedio de los elementos del arreglo es: " + promedio(a));
