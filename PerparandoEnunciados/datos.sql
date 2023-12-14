d00298646cb200f7507e308ac19c12932db5524d
prubioamzubiri <prubioam@zubirimanteo.com>
2023-12-14 12:06:18 +0000

    Add food data and exercise instructions

---
+++ PerparandoEnunciados/datos.sql
@@ -0,0 +1,22 @@
+INSERT into FOOD (name, healthy, fat, carbs, protein)
+values 
+('Apple', "Healthy", 0.2, 14, 0.3),
+('Chocolate', "Not Heathy",0.2, 52, 0.4)
+, ('Bread', "Not Heathy", 0.9, 49, 0.1)
+, ('Banana', "Healthy", 0.3, 23, 1.1)
+, ('Orange', "Healthy", 0.2, 12, 0.9)
+, ('Pasta', "Not Heathy", 0.9, 25, 0.1)
+, ('Rice', "Not Heathy", 0.9, 28, 0.3)
+, ('Potato', "Healthy", 0.1, 17, 0.6)
+, ('Tomato', "Healthy", 0.2, 3.9, 0.9)
+, ('Carrot', "Healthy", 0.2, 9.6, 0.6)
+, ('Cucumber', "Healthy", 0.1, 3.6, 0.6)
+, ('Lettuce', "Healthy", 0.1, 2.9, 1.4)
+, ('Cheese', "Not Heathy", 0.3, 1.3, 0.2)
+, ('Egg', "Healthy", 0.1, 0.7, 0.1)
+, ('Milk', "Healthy", 0.1, 4.7, 3.3);
+
+
+
+
+
---
+++ PerparandoEnunciados/enunciados.py
@@ -0,0 +1,13 @@
+-- Ejercicio 1 --
+-- Palabras clave --
+Clases abstractas
+Poliformismo
+Listas
+-- Enunciado --
+Crea una clase abstracta llamada "Persona" que tenga como atributos "nombre" y "apellido" y como métodos "saludar" y "despedirse".
+Crea una clase llamada "Alumno" que herede de "Persona" y que tenga como atributo "notas" y como métodos "estudiar" y "aprobar".
+Crea una clase llamada "Profesor" que herede de "Persona" y que tenga como atributo "materia" y como métodos "enseñar" y "calificar".
+Crea una lista de objetos de tipo "Persona" y llámala "personas".
+Agrega a la lista anterior un objeto de tipo "Alumno" y otro de tipo "Profesor".
+Itera la lista "personas" y por cada objeto de tipo "Persona" que encuentres, ejecuta su método "saludar".
+
